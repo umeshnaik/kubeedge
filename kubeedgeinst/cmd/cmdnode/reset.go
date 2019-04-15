74ad8b5bd9597f24f997aa8d8b243c25149e74a5
Manju <manju.srivatsa.kp@huawei.com>
2019-04-15 23:55:20 +0530

    skeleton code to implement Kubeedge Installer

---
+++ kubeedgeinst/cmd/cmdnode/reset.go
@@ -0,0 +1,41 @@
+// Copyright © 2019 Kubeedge Authors <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/spf13/cobra"
+)
+
+// resetCmd represents the reset command
+func NewCmdReset(out io.Writer) *cobra.Command {
+	var cmd = &cobra.Command{
+		Use:   "reset",
+		Short: "Teardowns cloud component",
+		Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO: Work your own magic here
+			fmt.Println("reset called")
+		},
+	}
+	return cmd
+}
